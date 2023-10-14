aeeb293886966f2e0e04204530259b5528d91752
Phiniey <phineasayub210@gmail.com>
2023-10-14 16:05:39 -0700

    3 colors, 10 digits, and 7 notes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to extract the last digit from
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int j;
+
+	{
+		j = n % 10;
+
+		if (j < 0)
+		{
+			j = -j;
+		}
+	}
+	_putchar('0' + j);
+	return (j);
+}
