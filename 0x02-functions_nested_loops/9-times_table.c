8f86901cf5d4067c70ac671c7e76e65891a1daa1
Phiniey <phineasayub210@gmail.com>
2023-10-14 17:04:51 -0700

    My  times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,30 +2,43 @@
 
 /**
  * times_table - prints the 9 times table
+ *
+ * Return: Always 0.
  */
+
 void times_table(void)
 {
 	int i;
 	int j;
+	int result;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0 ; i <= 9 ; i++)
 
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0 ; j <= 9 ; j++)
 
 		{
-			int result = i * j;
+			result = i * j;
 
 			if (j == 0)
 
 			{
 				_putchar('0' + result);
 			}
-			else
+			if (result < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + result);
+			}
+
+			else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
 		_putchar('\n');
