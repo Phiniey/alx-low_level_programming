b345a8c8c1e1c069ff20542a33dd6934369a1d3c
Phiniey <phineasayub210@gmail.com>
2023-10-16 19:19:06 -0700

    First pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * rest_to_98 -Updates an integer to the value 98.
+ * rest_to_98 - Updates an integer to the value 98.
  * @n: Pointer to the value to be changed
- * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
