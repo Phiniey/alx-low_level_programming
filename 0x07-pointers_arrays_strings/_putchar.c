9f10beea2ac3d295ca2f59fefa31dfb29bf7dd11
Phiniey <phineasayub210@gmail.com>
2023-10-23 20:21:25 -0700

    Chessbaord

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(int c)
+{
+    return write(1, &c, 1);
+}
+
