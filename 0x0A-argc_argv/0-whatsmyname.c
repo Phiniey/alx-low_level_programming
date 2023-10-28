f7eae91e45827332e16e04d901caed64be14be7d
Phiniey <phineasayub210@gmail.com>
2023-10-27 20:15:17 -0700

    Jj

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - function that prints pogram name
- * @arg: argum,ent count
- * @argv: argument vector
- * retrun: 0
+ * main - Function that prints the program name.
+ * It takes two arguments: argc (argument count) and argv (argument vector).
+ * It prints the program name and returns 0 to indicate successful execution.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
