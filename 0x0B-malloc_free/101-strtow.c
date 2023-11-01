80f0e3e0aeb20b2a6bdc0167ebb0c728b146d160
Phiniey <phineasayub210@gmail.com>
2023-11-01 15:25:50 -0700

    See

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,31 +11,46 @@
 
 char **strtow(char *str)
 {
-	int num_words = 0;
+	int num_words = 0, i;
 	char **words;
-	char *word = strtok(str, " ");
+	char *word;
+	char *str_copy = NULL;
 
 	if (str == NULL || str[0] == '\0')
 	{
 		return (NULL);
 	}
-	words = malloc(sizeof(char *) * (strlen(str) + 1));
-	if (words == NULL)
+
+	str_copy = strdup(str);
+	if (str_copy == NULL)
 	{
 		return (NULL);
 	}
+
+	words = malloc(sizeof(char *) * (strlen(str) + 1));
+	if (words == NULL)
+	{
+		free(str_copy);
+		return (NULL);
+	}
+
+	word = strtok(str_copy, " ");
 	while (word != NULL)
 	{
 		words[num_words] = strdup(word);
 		if (words[num_words] == NULL)
 		{
+			for (i = 0; i < num_words; i++)
+				 free(words[i]);
 			free(words);
+			free(str_copy);
 			return (NULL);
 		}
 		num_words++;
 		word = strtok(NULL, " ");
 	}
 	words[num_words] = NULL;
+	free(str_copy);
 	return (words);
 
 }
