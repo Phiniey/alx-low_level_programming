eb461e6fc2f8b01f9ae104d3e1ecc49104493acd
Phiniey <phineasayub210@gmail.com>
2023-11-07 18:32:04 -0800

    Printing with element nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,22 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d != 0)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		if (d->age < 0)
+		{
+			printf("Age: (nil)\n");
+		}
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
